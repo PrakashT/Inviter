91ba310510fc8959d65680e65c6ed5920ba91f9b
Manikanta Toram <manikantaprakashtoram@manikantas-macbook-pro.local>
2018-08-30 00:46:26 +0530

    Did Choose music VC design

--- Inviter/Controllers/ChooseMusicViewController.swift
+++ Inviter/Controllers/ChooseMusicViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 import Foundation
 
-class ChooseMusicViewController: UIViewController {
+class ChooseMusicViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var musicListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        musicListTableView.register(UINib.init(nibName: "ChooseMusicTableViewCell", bundle: nil), forCellReuseIdentifier: "ChooseMusicTableViewCellID")
+        
+//        musicListTableView.estimatedRowHeight = 1.0
+//        musicListTableView.rowHeight = UITableViewAutomaticDimension;
     }
+    
+    // MARK: - UITableView DataSources
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChooseMusicTableViewCellID", for: indexPath) as! ChooseMusicTableViewCell
+        
+        return cell
+    }
+    
     
     
 }
---
+++ Inviter/Views/TableViewCells/ChooseMusicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChooseMusicTableViewCell.swift
+//  Inviter
+//
+//  Created by Manikanta Prakash Toram on 8/30/18.
+//  Copyright © 2018 Manikanta Prakash Toram. All rights reserved.
+//
+
+import UIKit
+
+class ChooseMusicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
